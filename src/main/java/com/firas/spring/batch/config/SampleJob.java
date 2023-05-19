cb12017488eac4dec93b8c5aea4314e5a0ce71a7
firaschemak <firas.chemak@dti.ulaval.ca>
2023-05-19 16:38:14 -0400

    Tasklet & Chunk Oriented Steps Together

--- src/main/java/com/firas/spring/batch/config/SampleJob.java
+++ src/main/java/com/firas/spring/batch/config/SampleJob.java
@@ -1,16 +1,23 @@
 package com.firas.spring.batch.config;
 
 import com.firas.spring.batch.listener.FirstJobListener;
+import com.firas.spring.batch.processor.FirstItemProcessor;
+import com.firas.spring.batch.reader.FirstItemtReader;
+import com.firas.spring.batch.writer.FirstItemWriter;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.ExecutionContextSerializer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +29,8 @@ import org.springframework.transaction.PlatformTransactionManager;
  * @since {{next_version}}
  */
 @Configuration
+//@EnableBatchProcessing
+        //(executionContextSerializerRef = "jacksonSerializer")
 public class SampleJob {
 
     @Autowired
@@ -59,6 +68,29 @@ public class SampleJob {
     }
 
 
+    @Bean
+    public Job secondJob(JobRepository jobRepository, Step firstchunkStep,Step firstStep) {
+        return new JobBuilder("second_job", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(firstchunkStep)
+                .next(firstStep)
+                .build();
+    }
 
+    @Bean
+    Step firstchunkStep(JobRepository jobRepository, FirstItemtReader itemReader,
+            ItemProcessor firstItemProcessor , FirstItemWriter firstItemWriter) {
+        return new StepBuilder("first_chunk_step",jobRepository)
+                .<Integer,Long>chunk(3,transactionManager)
+                .reader(itemReader)
+                .processor(firstItemProcessor)
+                .writer(firstItemWriter)
+                .transactionManager(transactionManager).build();
+    }
+
+    /*@Bean
+    public ExecutionContextSerializer jacksonSerializer() {
+        return new Jackson2ExecutionContextStringSerializer();
+    }*/
 
 }
--- src/main/java/com/firas/spring/batch/lab/SpringBatchTutorielApplication.java
+++ src/main/java/com/firas/spring/batch/lab/SpringBatchTutorielApplication.java
@@ -1,14 +1,16 @@
 package com.firas.spring.batch.lab;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.repository.ExecutionContextSerializer;
+import org.springframework.batch.core.repository.dao.Jackson2ExecutionContextStringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@EnableBatchProcessing
 @ComponentScan({"com.firas.spring.batch.config","com.firas.spring.batch.service"
-        ,"com.firas.spring.batch.listener","com.firas.spring.batch.processor","com.firas.spring.batch.writer"})
+        ,"com.firas.spring.batch.listener","com.firas.spring.batch.reader","com.firas.spring.batch.processor","com.firas.spring.batch.writer"})
 public class SpringBatchTutorielApplication {
 
     public static void main(String[] args) {
